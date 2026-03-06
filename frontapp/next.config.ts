371930d7b713fa27fee94bc3608ebc8ed230e9ac
LUIS AUGUSTO TORRES CAVERO <ltorresca03@ucvvirtual.edu.pe>
2026-03-06 18:25:35 -0500

    Update next.config.ts

--- frontapp/next.config.ts
+++ frontapp/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.LARAVEL_API_URL}/:path*`,
+      },
+    ];
+  },
   onDemandEntries: {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 5,
@@ -13,54 +20,17 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'i.pravatar.cc' },
       { protocol: 'https', hostname: 'lyriumbiomarketplace.com' },
-      {
-        protocol: 'https',
-        hostname: '**.woocommerce.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.wp.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'secure.gravatar.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.lyrium.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lyrium.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lyriumbiomarketplace.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.lyriumbiomarketplace.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      },
+      { protocol: 'https', hostname: '**.woocommerce.com' },
+      { protocol: 'https', hostname: '**.wp.com' },
+      { protocol: 'https', hostname: 'secure.gravatar.com' },
+      { protocol: 'https', hostname: 'via.placeholder.com' },
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'localhost' },
+      { protocol: 'https', hostname: '*.lyrium.com' },
+      { protocol: 'https', hostname: 'lyrium.com' },
+      { protocol: 'https', hostname: 'lyriumbiomarketplace.com' },
+      { protocol: 'https', hostname: '**.lyriumbiomarketplace.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
 };
